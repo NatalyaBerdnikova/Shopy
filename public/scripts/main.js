b8d332662cd7295b4bea93c076a9707f5411c4b7
Natalya Berdnikova <kitakava@mail.ru>
2020-11-30 15:36:46 +0300

    Верстка секции product

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,32 +1,35 @@
 let priceRange = document.getElementById('price-range');
-let priceRangeData = {
-  min: Number(priceRange.getAttribute('min')),
-  max: Number(priceRange.getAttribute('max'))
-};
 
-noUiSlider.create(priceRange, {
-  start: [priceRangeData.min, priceRangeData.max],
-  step: 1,
-  connect: true,
-  tooltips: [true, true],
-  format: {
-    to: function (value) {
-        return Math.trunc(value) + '$';
+if (priceRange) {
+  let priceRangeData = {
+    min: Number(priceRange.getAttribute('min')),
+    max: Number(priceRange.getAttribute('max'))
+  };
+
+  noUiSlider.create(priceRange, {
+    start: [priceRangeData.min, priceRangeData.max],
+    step: 1,
+    connect: true,
+    tooltips: [true, true],
+    format: {
+      to: function (value) {
+          return Math.trunc(value) + '$';
+      },
+      from: function (value) {
+          return Number(value);
+      }
     },
-    from: function (value) {
-        return Number(value);
+    range: {
+      'min': [priceRangeData.min],
+      'max': [priceRangeData.max]
     }
-  },
-  range: {
-    'min': [priceRangeData.min],
-    'max': [priceRangeData.max]
-  }
-});
+  });
 
-let priceFrom = document.getElementById('price-from');
-let priceTo = document.getElementById('price-to');
+  let priceFrom = document.getElementById('price-from');
+  let priceTo = document.getElementById('price-to');
 
-priceRange.noUiSlider.on('update', function (values, handle) {
-  let priceInput = handle ? priceTo : priceFrom;
-  priceInput.value = Math.trunc(values[handle].replace('$', ''));
-});
+  priceRange.noUiSlider.on('update', function (values, handle) {
+    let priceInput = handle ? priceTo : priceFrom;
+    priceInput.value = Math.trunc(values[handle].replace('$', ''));
+  });
+}
